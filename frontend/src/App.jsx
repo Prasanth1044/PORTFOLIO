31b2b57145268a75b95bbae09c36e12ba2acdc30
Prasanth1044 <pras261105@gmail.com>
2026-01-05 07:38:16 +0530

    remHom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import DarkVeil from './components/DarkVeil';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './styles/main.css';
 
 const App = () => {
@@ -28,7 +27,7 @@ const App = () => {
         />
       </div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/about" replace />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
